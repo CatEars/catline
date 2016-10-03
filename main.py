93833d74ccb4899d4522dc56eef04900b45ff53c
Henke Adolfsson <henad221@student.liu.se>
2016-10-03 20:37:15 +0200

    Refactor + worked out better syntax coloring

--- main.py
+++ main.py
@@ -2,99 +2,8 @@
 
 import sys
 import os
+import writers
 
-from pygments import lexers, formatters, highlight
-
-def iter_lines(fname, start, end):
-    with open(fname, 'r') as f:
-        for idx, line in enumerate(f):
-            if end != -1 and end <= idx:
-                break
-            if start <= idx:
-                yield line.rstrip()
-
-# A class for wrapping the functionality requested from pygments
-class PygmentWrapper:
-
-    def __init__(self, fname, start, end):
-        self._start = start
-        self._end = end
-        self.fname = fname
-
-    def init_writer(self):
-        self.lexer = lexers.get_lexer_for_filename(self.fname)
-        self.formatter = formatters.get_formatter_by_name('terminal')
-        self.lines = list(iter_lines(self.fname, self._start, self._end))
-        
-    def format_line(self, linenum):
-        idx = linenum - self._start
-        if 0 <= idx < len(self.lines):
-            return highlight(self.lines[idx], self.lexer, self.formatter).rstrip()
-        return None
-
-    @property
-    def start(self):
-        return self._start
-
-    @property
-    def end(self):
-        return self._start + len(self.lines)
-
-def match_py(fname):
-    return lexers.get_lexer_by_name('python') if fname.endswith('.py') else None
-
-def match_cpp(fname):
-    m = fname.endswith
-    if m('.cpp') or m('hpp') or m('.cc') or m('hh') or \
-       m('.cxx') or m('.hxx') or m('.c++') or m('.h++'):
-        return lexers.get_lexer_by_name('c++')
-    return None
-
-def match_java(fname):
-    return lexers.get_lexer_by_name('java') if fname.endswith('.java') else None
-
-class FastPygmentWrapper(PygmentWrapper):
-    """Doesn't do any fancy matching, just uses the most common of languages
-    with their most common fileendings."""
-
-    MATCHERS = [match_py, match_cpp, match_java]
-    
-    @staticmethod
-    def get_lexer_for_filename(fname):
-        for matcher in FastPygmentWrapper.MATCHERS:
-            if matcher(fname):
-                return matcher(fname)
-        return lexers.TextLexer()
-    
-    def init_writer(self):
-        self.lexer = FastPygmentWrapper.get_lexer_for_filename(self.fname)
-        self.formatter = formatters.get_formatter_by_name('terminal')
-        self.lines = list(iter_lines(self.fname, self._start, self._end))
-    
-class NormalWriter:
-
-    def __init__(self, fname, start, end):
-        self._start = start
-        self._end = end
-        self.fname = fname
-        self.lines = []
-
-    def init_writer(self):
-        self.lines = list(iter_lines(self.fname, self._start, self._end))
-
-    def format_line(self, linenum):
-        idx = linenum - self._start
-        if 0 <= idx < len(self.lines):
-            return self.lines[idx]
-        return None
-
-    @property
-    def start(self):
-        return self._start
-
-    @property
-    def end(self):
-        return self._start + len(self.lines)
     
 def print_help():
     print('Usage: catline file [ a:b | a b ]')
@@ -131,7 +40,7 @@ def find_start_and_end(start, end):
 
 def catline_args(fname, start=0, end=-1):
     start, end = find_start_and_end(start, end)
-    writer = FastPygmentWrapper(fname, start, end)
+    writer = writers.FastPygmentWrapper(fname, start, end)
     catline(writer, fname, start, end)
 
     
@@ -141,5 +50,6 @@ def main():
     else:
         print_help()
 
+        
 if __name__ == '__main__':
     main()
---
+++ writers.py
@@ -0,0 +1,96 @@
+from pygments import lexers, formatters, highlight
+
+def iter_lines(fname, start, end):
+    with open(fname, 'r') as f:
+        for idx, line in enumerate(f):
+            if end != -1 and end <= idx:
+                break
+            if start <= idx:
+                yield line
+    
+class NormalWriter: # Base class / interface
+
+    def __init__(self, fname, start, end):
+        self._start = start
+        self._end = end
+        self.fname = fname
+        self.lines = []
+
+    def init_writer(self):
+        self.lines = list(x.rstrip() for x in iter_lines(self.fname, self._start, self._end))
+
+    def format_line(self, linenum):
+        idx = linenum - self._start
+        if 0 <= idx < len(self.lines):
+            return self.lines[idx]
+        return None
+
+    @property
+    def start(self):
+        return self._start
+
+    @property
+    def end(self):
+        return self._start + len(self.lines) - 1
+                
+# A class for wrapping the functionality requested from pygments
+class PygmentWrapper:
+
+    def __init__(self, fname, start, end):
+        self._start = start
+        self._end = end
+        self.fname = fname
+
+    def get_lexer(self):
+        return lexers.get_lexer_by_filename(self.fname)
+        
+    def init_writer(self):
+        self.lexer = self.get_lexer()
+        self.formatter = formatters.get_formatter_by_name('terminal')
+        self.lines = "".join(iter_lines(self.fname, 0, -1))
+        self.lines = highlight(self.lines, self.lexer, self.formatter).split('\n')
+        
+    def format_line(self, linenum):
+        if 0 <= linenum < len(self.lines):
+            return self.lines[linenum]
+        return None
+
+    @property
+    def start(self):
+        return self._start
+
+    @property
+    def end(self):
+        if self._end == -1:
+            return len(self.lines)
+        else:
+            return self._end
+
+
+### Matchers for fast pygment wrapper ###
+
+def match_py(fname):
+    return lexers.get_lexer_by_name('python') if fname.endswith('.py') else None
+
+def match_cpp(fname):
+    m = fname.endswith
+    if m('.cpp') or m('hpp') or m('.cc') or m('hh') or \
+       m('.cxx') or m('.hxx') or m('.c++') or m('.h++'):
+        return lexers.get_lexer_by_name('c++')
+    return None
+
+def match_java(fname):
+    return lexers.get_lexer_by_name('java') if fname.endswith('.java') else None
+
+
+class FastPygmentWrapper(PygmentWrapper):
+    """Doesn't do any fancy matching, just uses the most common of languages
+    with their most common fileendings."""
+
+    MATCHERS = [match_py, match_cpp, match_java]
+
+    def get_lexer(self):
+        for matcher in FastPygmentWrapper.MATCHERS:
+            if matcher(self.fname):
+                return matcher(self.fname)
+        return lexers.TextLexer()
